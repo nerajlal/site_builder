ded97b3a35bb7cc3c18cdd34696b3b90778cee12
neraj <nerajnerajlal@gmail.com>
2025-08-16 15:26:00 +0530

    product single on templates

--- routes/web.php
+++ routes/web.php
@@ -75,10 +75,10 @@ Route::get('/product1', [ProductController1::class, 'index'])->name('template1.p
 Route::get('/product2', [ProductController2::class, 'index'])->name('template2.product2');
 Route::get('/product3', [ProductController3::class, 'index'])->name('template3.product3');
 Route::get('/product4', [ProductController4::class, 'index'])->name('template4.product4');
-Route::get('/single-product1/{productId}', [ProductController1::class, 'index'])->name('template1.single-product1');
-Route::get('/single-product2/{productId}', [ProductController2::class, 'index'])->name('template2.single-product2');
-Route::get('/single-product3/{productId}', [ProductController3::class, 'index'])->name('template3.single-product3');
-Route::get('/single-product4/{productId}', [ProductController4::class, 'index'])->name('template4.single-product4');
+Route::get('/single-product1', [ProductController1::class, 'index'])->name('template1.single-product1');
+Route::get('/single-product2', [ProductController2::class, 'index'])->name('template2.single-product2');
+Route::get('/single-product3', [ProductController3::class, 'index'])->name('template3.single-product3');
+Route::get('/single-product4', [ProductController4::class, 'index'])->name('template4.single-product4');
 
 // Customer-facing product routes with header_footer_id
 Route::get('/product1/{headerFooterId}', [ProductController1::class, 'showCustomer'])->name('template1.product1.customer');
