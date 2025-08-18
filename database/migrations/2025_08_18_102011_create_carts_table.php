f4af097cdc618250483a35a67f59b504a12157a6
neraj <nerajnerajlal@gmail.com>
2025-08-18 15:55:52 +0530

    cart

---
+++ database/migrations/2025_08_18_102011_create_carts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('header_footer_id'); // The ID of the store
+            $table->foreignId('site_customer_id')->nullable()->constrained('site_customers')->onDelete('cascade'); // The ID of the logged-in user
+            $table->string('session_id')->nullable(); // The ID for guest carts
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+            $table->unsignedInteger('quantity');
+            $table->json('options')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('carts');
+    }
+};
